251ef30a9663e8c8ff1e6040c7071375410401a0
Sandeep Madineni <smadine@conviva.com>
2022-10-20 17:46:09 +0530

    Release 4.0.3

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ConvivaIMASDK",
-            url: "https://github.com/Conviva/conviva-ios-imasdk/raw/4.0.2/Framework/ConvivaIMASDK.xcframework.zip",
-            checksum: "2394326a7ad5deed93a35114e514305b2535f290fb18f5be6aab620f8d33b8a3"),
+            url: "https://github.com/Conviva/conviva-ios-imasdk/raw/4.0.3/Framework/ConvivaIMASDK.xcframework.zip",
+            checksum: "68db2f6bf9d2b4772c67b57f06c32853c2b5290883407d89b73dbdf8523b17ef"),
         
         .binaryTarget(
             name: "ConvivaSDK",
-            url: "https://github.com/Conviva/ConvivaSDK/raw/4.0.26/Framework/ConvivaSDK.xcframework.zip",
-            checksum: "5a4c385fc52f2f4c6a02c6c345af81aa6ff836750080f856d702b7f3834c9a15"),
+            url: "https://github.com/Conviva/ConvivaSDK/raw/4.0.30/Framework/ConvivaSDK.xcframework.zip",
+            checksum: "db1beee20458b23c422cbe66785d0a0a2b933a0f4590b10ef25fdf0e51b1bc57"),
 
         .target(
               name: "ConvivaIMASDKTarget",
